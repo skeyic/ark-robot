dcaefb4f3096271b1e863e8a0e3e0aa6e9a3e985
skeyic <skeyic@me.com>
2021-03-15 20:56:53 +0800

    Fresh init the master at the beginning.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/golang/glog"
 	"github.com/skeyic/ark-robot/app/service"
 )
@@ -12,6 +13,10 @@ func main() {
 	//)
 
 	flag.Parse()
+	err := service.TheMaster.FreshInit()
+	if err != nil {
+		panic(fmt.Sprintf("master failed to fresh init, err: %v", err))
+	}
 	service.TheMaster.StartDownload()
 
 	glog.V(4).Info("Ark robot starts...")
